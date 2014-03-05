92e48c60efeb9721092520a9b9fbbe45a7d2dc20
malick youla <myoula@videotron.ca>
2014-03-05 08:38:57 +0100

    Initial Commit

---
+++ TunedIn test/TIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TIAppDelegate.h
+//  TunedIn test
+//
+//  Created by Malick Youla on 2014-03-05.
+//  Copyright (c) 2014 Malick Youla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
